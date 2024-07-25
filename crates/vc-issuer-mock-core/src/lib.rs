35ec4b83580c14a3e14cfe93f1bd66d2d72b553d
Sho Nakatani <lay.sakura@gmail.com>
2024-07-25 17:22:21 +0900

    test: move parse error tests to lib.rs

--- crates/vc-issuer-mock-core/src/lib.rs
+++ crates/vc-issuer-mock-core/src/lib.rs
@@ -26,8 +26,9 @@ pub mod test_vc_json;
 #[cfg(test)]
 mod tests {
     use crate::{
-        test_issuer_keys::jwk_p384, test_tracing::init_tracing,
-        vcdm_v2::problem_details::PredefinedProblemType,
+        test_issuer_keys::jwk_p384,
+        test_tracing::init_tracing,
+        vcdm_v2::problem_details::{PredefinedProblemType, ProblemType},
     };
 
     use super::*;
@@ -40,8 +41,13 @@ mod tests {
     use serde_json::Value;
     use tower::ServiceExt;
 
-    #[tokio::test]
-    async fn test_non_json_req_error_res() {
+    /// Although most of the tests to API endpoints should be done in `crate::endpoints`,
+    /// tests in `crate::endpoints` require JSON serialization before calling endpoints.
+    /// Thus, to test the responses of serialization-related errors, we need to test here.
+    async fn test_issue_req_serialize_error<T: ProblemType>(
+        req_body: &'static str,
+        expected_problem_type: T,
+    ) {
         init_tracing();
 
         let issuer_keys = jwk_p384();
@@ -51,7 +57,7 @@ mod tests {
             .method("POST")
             .uri("/credentials/issue")
             .header("content-type", "application/json")
-            .body(Body::from("INVALID-AS-JSON"))
+            .body(Body::from(req_body))
             .unwrap();
 
         let res = app.oneshot(req).await.unwrap();
@@ -68,6 +74,247 @@ mod tests {
         assert_eq!(status, 400);
 
         let r#type = json["problemDetails"]["type"].as_str().unwrap();
-        assert_eq!(r#type, PredefinedProblemType::ParsingError.to_string());
+        assert_eq!(r#type, expected_problem_type.to_string());
+    }
+
+    #[tokio::test]
+    async fn test_issue_non_json_req_error_res() {
+        test_issue_req_serialize_error("INVALID-AS-JSON", PredefinedProblemType::ParsingError)
+            .await;
+    }
+
+    #[tokio::test]
+    async fn test_issue_parsing_error_context_unexpected_url() {
+        test_issue_req_serialize_error(
+            r#"
+{
+  "credential": {
+    "@context": [
+      "https://example.com/INVALID_CONTEXT"
+    ],
+    "id": "http://university.example/credentials/1872",
+    "type": ["VerifiableCredential", "ExampleAlumniCredential"],
+    "issuer": "https://university.example/issuers/565049",
+    "validFrom": "2023-07-01T19:23:24Z",
+    "credentialSubject": {
+      "id": "did:example:ebfeb1f712ebc6f1c276e12ec21",
+      "alumniOf": {
+        "id": "did:example:c276e12ec21ebfeb1f712ebc6f1",
+        "name": "Example University"
+      }
+    }
+  },
+  "options": {}
+}"#,
+            PredefinedProblemType::ParsingError,
+        )
+        .await;
+    }
+
+    #[tokio::test]
+    async fn test_issue_parsing_error_context_not_url() {
+        test_issue_req_serialize_error(
+            r#"
+{
+  "credential": {
+    "@context": [
+      "v2"
+    ],
+    "id": "http://university.example/credentials/1872",
+    "type": ["VerifiableCredential", "ExampleAlumniCredential"],
+    "issuer": "https://university.example/issuers/565049",
+    "validFrom": "2023-07-01T19:23:24Z",
+    "credentialSubject": {
+      "id": "did:example:ebfeb1f712ebc6f1c276e12ec21",
+      "alumniOf": {
+        "id": "did:example:c276e12ec21ebfeb1f712ebc6f1",
+        "name": "Example University"
+      }
+    }
+  },
+  "options": {}
+}"#,
+            PredefinedProblemType::ParsingError,
+        )
+        .await;
+    }
+
+    /// <https://www.w3.org/TR/vc-data-model-2.0/#identifiers>
+    ///
+    /// > If present, the value of the id property MUST be a single URL, which MAY be dereferenceable.
+    #[tokio::test]
+    async fn test_issue_malformed_value_error_id_not_url() {
+        test_issue_req_serialize_error(
+            r#"
+{
+  "credential": {
+    "@context": [
+      "https://www.w3.org/ns/credentials/v2",
+      "https://www.w3.org/ns/credentials/examples/v2"
+    ],
+    "id": "INVALID_ID",
+    "type": ["VerifiableCredential", "ExampleAlumniCredential"],
+    "issuer": "https://university.example/issuers/565049",
+    "validFrom": "2023-07-01T19:23:24Z",
+    "credentialSubject": {
+      "id": "did:example:ebfeb1f712ebc6f1c276e12ec21",
+      "alumniOf": {
+        "id": "did:example:c276e12ec21ebfeb1f712ebc6f1",
+        "name": "Example University"
+      }
+    }
+  },
+  "options": {}
+}"#,
+            PredefinedProblemType::ParsingError,
+        )
+        .await
+    }
+
+    /// <https://www.w3.org/TR/vc-data-model-2.0/#types>
+    ///
+    /// > The value of the type property MUST be one or more terms and/or...
+    #[tokio::test]
+    async fn test_issue_malformed_value_error_empty_type() {
+        test_issue_req_serialize_error(
+            r#"
+{
+  "credential": {
+    "@context": [
+      "https://www.w3.org/ns/credentials/v2",
+      "https://www.w3.org/ns/credentials/examples/v2"
+    ],
+    "id": "http://university.example/credentials/1872",
+    "type": [],
+    "issuer": "https://university.example/issuers/565049",
+    "validFrom": "2023-07-01T19:23:24Z",
+    "credentialSubject": {
+      "id": "did:example:ebfeb1f712ebc6f1c276e12ec21",
+      "alumniOf": {
+        "id": "did:example:c276e12ec21ebfeb1f712ebc6f1",
+        "name": "Example University"
+      }
+    }
+  },
+  "options": {}
+}"#,
+            PredefinedProblemType::ParsingError,
+        )
+        .await
+    }
+
+    /// <https://www.w3.org/TR/vc-data-model-2.0/#issuer>
+    ///
+    /// > The value of the issuer property MUST be either a URL, or an object containing an id property whose value is a URL;
+    #[tokio::test]
+    async fn test_issue_malformed_value_error_issuer_not_url() {
+        test_issue_req_serialize_error(
+            r#"
+{
+  "credential": {
+    "@context": [
+      "https://www.w3.org/ns/credentials/v2",
+      "https://www.w3.org/ns/credentials/examples/v2"
+    ],
+    "id": "http://university.example/credentials/1872",
+    "type": ["VerifiableCredential", "ExampleAlumniCredential"],
+    "issuer": "INVALID_ISSUER",
+    "validFrom": "2023-07-01T19:23:24Z",
+    "credentialSubject": {
+      "id": "did:example:ebfeb1f712ebc6f1c276e12ec21",
+      "alumniOf": {
+        "id": "did:example:c276e12ec21ebfeb1f712ebc6f1",
+        "name": "Example University"
+      }
+    }
+  },
+  "options": {}
+}"#,
+            PredefinedProblemType::ParsingError,
+        )
+        .await
+    }
+
+    /// Date and time should be separated by `T` instead of a space.
+    #[tokio::test]
+    async fn test_issue_malformed_value_error_valid_from_invalid() {
+        test_issue_req_serialize_error(
+            r#"
+{
+  "credential": {
+    "@context": [
+      "https://www.w3.org/ns/credentials/v2",
+      "https://www.w3.org/ns/credentials/examples/v2"
+    ],
+    "id": "http://university.example/credentials/1872",
+    "type": ["VerifiableCredential", "ExampleAlumniCredential"],
+    "issuer": "https://university.example/issuers/565049",
+    "validFrom": "2023-07-01 19:23:24Z",
+    "credentialSubject": {
+      "id": "did:example:ebfeb1f712ebc6f1c276e12ec21",
+      "alumniOf": {
+        "id": "did:example:c276e12ec21ebfeb1f712ebc6f1",
+        "name": "Example University"
+      }
+    }
+  },
+  "options": {}
+}"#,
+            PredefinedProblemType::ParsingError,
+        )
+        .await
+    }
+
+    /// Date and time should be separated by `T` instead of a space.
+    #[tokio::test]
+    async fn test_issue_malformed_value_error_valid_until_invalid() {
+        test_issue_req_serialize_error(
+            r#"
+{
+  "credential": {
+    "@context": [
+      "https://www.w3.org/ns/credentials/v2",
+      "https://www.w3.org/ns/credentials/examples/v2"
+    ],
+    "id": "http://university.example/credentials/1872",
+    "type": ["VerifiableCredential", "ExampleAlumniCredential"],
+    "issuer": "https://university.example/issuers/565049",
+    "validUntil": "2023-07-01 19:23:24Z",
+    "credentialSubject": {
+      "id": "did:example:ebfeb1f712ebc6f1c276e12ec21",
+      "alumniOf": {
+        "id": "did:example:c276e12ec21ebfeb1f712ebc6f1",
+        "name": "Example University"
+      }
+    }
+  },
+  "options": {}
+}"#,
+            PredefinedProblemType::ParsingError,
+        )
+        .await
+    }
+
+    #[tokio::test]
+    async fn test_issue_malformed_value_error_credential_subject_empty() {
+        test_issue_req_serialize_error(
+            r#"
+{
+  "credential": {
+    "@context": [
+      "https://www.w3.org/ns/credentials/v2",
+      "https://www.w3.org/ns/credentials/examples/v2"
+    ],
+    "id": "http://university.example/credentials/1872",
+    "type": ["VerifiableCredential", "ExampleAlumniCredential"],
+    "issuer": "https://university.example/issuers/565049",
+    "validUntil": "2023-07-01T19:23:24Z",
+    "credentialSubject": []
+  },
+  "options": {}
+}"#,
+            PredefinedProblemType::ParsingError,
+        )
+        .await
     }
 }
