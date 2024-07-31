da7571b394679845935e1cd47c615d43f0fa4d45
Sho Nakatani <lay.sakura@gmail.com>
2024-07-31 12:00:13 +0900

    build: read signing keys from Render's secret files

--- docker/vc-issuer-mock-core/run_servers.sh
+++ docker/vc-issuer-mock-core/run_servers.sh
@@ -5,7 +5,16 @@ nginx &
 # runs on port 3000
 /usr/local/bin/didkit-http &
 
-# runs on port 40080
+# Run the vc-issuer-mock-core server
+## Read signing keys from files the Render platform places.
+for secret in ISSMOCK_PRIV_EC_P384 ISSMOCK_PRIV_OKP_ED25519; do
+    file="/etc/secrets/$secret"
+    if [ -r "$file" ]; then
+        export $secret=$(cat "$file")
+        echo "$secret has been set from $file"
+    fi
+done
+## runs on port 40080
 /usr/local/bin/vc-issuer-mock-core &
 
 wait # until all backgroup processes are finished (infinite)
